76fb7ce92abc1760805a82898adefb92bc4c0b43
xxsnzw <xxsnzw@126.com>
2016-07-12 15:25:01 +0800

    newproject

---
+++ releasetools.py
@@ -0,0 +1,18 @@
+import common
+import edify_generator
+import copy
+
+
+def ReplaceAssertions(info):
+    edify = info.script
+    for i in xrange(len(edify.script)):
+        if 'show_progress(0.750000, 0);' in edify.script[i]:
+            edify.script[i] = edify.script[i].replace('show_progress(0.750000, 0);', '''show_progress(0.750000, 0);
+ifelse(is_mounted("/system"), unmount("/system"));
+ui_print("Installing system, please wait a few minutes ... ");''')
+
+
+def FullOTA_InstallEnd(info):
+    ReplaceAssertions(info)
+    return
+
